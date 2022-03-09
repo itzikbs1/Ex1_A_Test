8552cc384e157022010ef0225c944cd53352e764
itzikbs1 <92460446+itzikbs1@users.noreply.github.com>
2022-03-09 17:20:19 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <stdlib.h>
+#include "mat.hpp"
+
+using namespace std;
+namespace ariel{
+    string mat(int col, int row, char c1, char c2){
+        return "";
+    }
+}
